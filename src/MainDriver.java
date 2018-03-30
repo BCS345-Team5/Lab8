409244e2d6ef55a4140c5919146f62cc479464df
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2018-03-29 20:27:17 -0400

    Initial commit

---
+++ src/MainDriver.java
@@ -0,0 +1,56 @@
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MainDriver extends Application {
+
+	public static double WINDOW_SIZE_X = 375;
+	public static double WINDOW_SIZE_Y = 375;
+	public static double WINDOW_CENTER_X = WINDOW_SIZE_X / 2;
+	public static double WINDOW_CENTER_Y = WINDOW_SIZE_Y / 2;
+	public final static double CIRCLE_WINDOW_RATIO = 2.0 / 3.0;
+
+	Scene scene;
+	SuperCircle myCircle;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		double calculatedRatio = WINDOW_SIZE_X * CIRCLE_WINDOW_RATIO / 2;
+
+		myCircle = new SuperCircle((int) (calculatedRatio));
+		
+		StringProperty sp = new SimpleStringProperty("Radius: ");
+		Text radiusLabel = new Text(10, 15, "");
+		radiusLabel.textProperty().bind(sp.concat(myCircle.getRadiusProperty()));
+
+		Pane root = new Pane(myCircle.getCircle(), radiusLabel);
+
+		scene = new Scene(root, WINDOW_SIZE_X, WINDOW_SIZE_Y);
+
+		scene.widthProperty().addListener(this::sceneSizeListener);
+		scene.heightProperty().addListener(this::sceneSizeListener);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
+
+	private void sceneSizeListener(ObservableValue<? extends Number> val, Number oldValue, Number newValue) {
+		WINDOW_SIZE_X = scene.getWidth();
+		WINDOW_SIZE_Y = scene.getHeight();
+		WINDOW_CENTER_X = WINDOW_SIZE_X / 2;
+		WINDOW_CENTER_Y = WINDOW_SIZE_Y / 2;
+
+		myCircle.updatePos(WINDOW_CENTER_X, WINDOW_CENTER_Y);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/SuperCircle.java
@@ -0,0 +1,97 @@
+import java.util.Random;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+public class SuperCircle {
+	private Random gen = new Random();
+	private Circle circle;
+	private int radius;
+	private double centerX = MainDriver.WINDOW_CENTER_X;
+	private double centerY = MainDriver.WINDOW_CENTER_Y;
+	private Point center = new Point();
+
+	private circlePoint[] points = new circlePoint[3];
+	private Line[] lines = new Line[3];
+
+	public SuperCircle() {
+		radius = 50;
+		init();
+	}
+
+	public SuperCircle(int radius) {
+		this.radius = radius;
+		init();
+	}
+
+	public DoubleProperty getRadiusProperty() {
+		return circle.radiusProperty();
+	}
+
+	private void init() {
+		circle = new Circle(centerX, centerY, radius);
+		circle.setFill(Color.TRANSPARENT);
+		circle.setStroke(Color.BLACK);
+		circle.setStrokeWidth(2);
+	}
+
+	public Group getCircle() {
+		Group tmp = new Group(circle);
+
+		for (int i = 0; i < 3; i++)
+			points[i] = genPoint();
+
+		for (int i = 0; i < 3; i++) {
+			lines[i] = new Line();
+			lines[i].startXProperty().bind(points[i].getXProperty());
+			lines[i].startYProperty().bind(points[i].getYProperty());
+			
+			if (i == 2) {
+				lines[i].endXProperty().bind(points[0].getXProperty());
+				lines[i].endYProperty().bind(points[0].getYProperty());
+			} 
+			else {
+				lines[i].endXProperty().bind(points[i + 1].getXProperty());
+				lines[i].endYProperty().bind(points[i + 1].getYProperty());
+			}
+
+		}
+		tmp.getChildren().addAll(lines);
+		for (int i = 0; i < 3; i++) 
+			tmp.getChildren().add(points[i].getCircle());
+
+		return tmp;
+	}
+
+	private circlePoint genPoint() {
+		double tmpX = gen.nextInt(radius * 2) - radius;
+		double tmpY = circlePoint.genYCoord(tmpX, radius);
+		if (tmpX % 2 != 0)
+			tmpY = tmpY * -1;
+		return new circlePoint(tmpX, tmpY, radius);
+	}
+
+	public void updatePos(double newCenterX, double newCenterY) {
+		centerX = MainDriver.WINDOW_CENTER_X;
+		centerY = MainDriver.WINDOW_CENTER_Y;
+
+		if (MainDriver.WINDOW_SIZE_X < MainDriver.WINDOW_SIZE_Y)
+			radius = (int) (MainDriver.WINDOW_SIZE_X * MainDriver.CIRCLE_WINDOW_RATIO / 2);
+		else
+			radius = (int) (MainDriver.WINDOW_SIZE_Y * MainDriver.CIRCLE_WINDOW_RATIO / 2);
+
+		circle.setCenterX(centerX);
+		circle.setCenterY(centerY);
+		circle.setRadius(radius);
+
+		for (circlePoint p : points) {
+			p.updatePos(radius);
+
+		}
+
+	}
+
+}
